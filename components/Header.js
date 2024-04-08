549cf0198b044d8af6550b117c51600e2ab61c9d
sharojb <sharojb@hotmail.com>
2024-04-08 00:14:10 -0400

    menu

--- components/Header.js
+++ components/Header.js
@@ -5,14 +5,14 @@ const Header = ({ onSearch }) => {
   const [showMenu, setShowMenu] = useState(false);
 
   const toggleMenu = () => {
-    setShowMenu(!showMenu);
+    document.querySelector("#hamburger-buttons").classList.toggle('header-buttons-active');
   };
 
   return (
     <header className="header" style={{ backgroundImage: "url('/bg1.png')" }}>
     <div>
     <label className="hamburger-menu" onClick={toggleMenu}>&#9776;</label>
-      <div className="header-buttons">
+      <div className="header-buttons" id="hamburger-buttons">
            <button className="button-ind">About</button>
           <button className="button-ind">Contact</button>
     </div>
