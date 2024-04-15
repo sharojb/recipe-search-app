969cfe382927e5e5b832739df9a7ba03b7c88805
sharojb <sharojb@hotmail.com>
2024-04-15 18:40:23 -0400

    forms

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,24 @@
-import React, { useState } from 'react';
-import SearchBar from './SearchBar';
+import React, { useState } from "react";
+import SearchBar from "./SearchBar";
 
 const Header = ({ onSearch }) => {
   const [showMenu, setShowMenu] = useState(false);
-  const [showLoginForm, setShowLoginForm] = useState(false); 
+  const [showLoginForm, setShowLoginForm] = useState(false);
 
   const toggleMenu = () => {
     setShowMenu(!showMenu);
   };
 
   const scrollToAbout = () => {
-    document.querySelector('.body-content').scrollIntoView({ behavior: 'smooth' });
+    document
+      .querySelector(".body-content")
+      .scrollIntoView({ behavior: "smooth" });
   };
 
   const scrollToContact = () => {
-    document.querySelector('.footer-text').scrollIntoView({ behavior: 'smooth' });
+    document
+      .querySelector(".footer-text")
+      .scrollIntoView({ behavior: "smooth" });
   };
 
   const toggleLoginForm = () => {
@@ -24,11 +28,22 @@ const Header = ({ onSearch }) => {
   return (
     <header className="header" style={{ backgroundImage: "url('/bg1.png')" }}>
       <div>
-        <label className="hamburger-menu" onClick={toggleMenu}>&#9776;</label>
-        <div className={`header-buttons ${showMenu ? 'header-buttons-active' : ''}`} id="hamburger-buttons">
-          <button className="button-ind" onClick={scrollToAbout}>About</button>
-          <button className="button-ind" onClick={scrollToContact}>Contact</button>
-          <button className="button-ind" onClick={toggleLoginForm}>Log In</button> 
+        <label className="hamburger-menu" onClick={toggleMenu}>
+          &#9776;
+        </label>
+        <div
+          className={`header-buttons ${showMenu ? "header-buttons-active" : ""}`}
+          id="hamburger-buttons"
+        >
+          <button className="button-ind" onClick={scrollToAbout}>
+            About
+          </button>
+          <button className="button-ind" onClick={scrollToContact}>
+            Contact
+          </button>
+          <button className="button-ind" onClick={toggleLoginForm}>
+            Log In
+          </button>
         </div>
       </div>
       <div className="logo-container">
@@ -37,14 +52,16 @@ const Header = ({ onSearch }) => {
       <section className="search-section">
         <SearchBar onSearch={onSearch} />
       </section>
-      {showLoginForm && ( 
-        <div className="login-form">
+      {showLoginForm && (
+        <div className="loginContainer">
           <form>
-            <label>Email:</label>
-            <input type="email" />
-            <label>Password:</label>
-            <input type="password" />
-            <button type="submit">Submit</button>
+            <label className="login">Email:</label>
+            <input className="inputLogin" type="email" />
+            <label className="login">Password:</label>
+            <input className="inputLogin" type="password" />
+            <button className="loginSubmit" type="submit">
+              Submit
+            </button>
           </form>
         </div>
       )}
