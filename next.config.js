6f6d8e5987bf3460c5b1f68d82931b17f7c4f332
sharojb <sharojb@hotmail.com>
2024-04-24 16:11:37 -0400

    gitignore

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ module.exports = {
   },
 
   env: {
-    API_KEY: '4e1ab513731c4ffeaa22089bd7a2d2a3',
+    NEXT_PUBLIC_SPOONACULAR_API_KEY: '4e1ab513731c4ffeaa22089bd7a2d2a3',
     FAST_REFRESH: 'false',
     CHOKIDAR_USEPOLLING: 'true',
     WATCHPACK_POLLING:'true',
