1cc99134dab006af6dae2d953ccc7666e87f28b6
sharojb <sharojb@hotmail.com>
2024-04-28 18:34:44 -0400

    regapi

--- AuthContext.js
+++ AuthContext.js
@@ -36,7 +36,7 @@ export const AuthProvider = ({ children }) => {
   const register = async (username, email, password) => {
     try {
       const response = await fetch(
-        `/api/register`,
+        `/api/userRoutes`,
         {
           method: "POST",
           headers: {
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
-const User = require('../models/User');
+const User = require('../../models/User');
 
 const router = express.Router();
 
