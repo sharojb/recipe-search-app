2c8f651a6d7e02260b38f4b1b0b36274c1d43a72
sharojb <sharojb@hotmail.com>
2024-04-28 19:54:43 -0400

    push

--- components/CreateProfileForm.jsx
+++ components/CreateProfileForm.jsx
@@ -19,7 +19,7 @@ const CreateProfileForm = () => {
     console.log("Password", password);
 
     try {
-      const response = await fetch(`http://localhost:3000/api/register`, {
+      const response = await fetch(`/api/register`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
